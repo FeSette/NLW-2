7ee2446477c50d3ee42e53bcb8fa475903c6a0e7
Felipe Sette <felipe_sette@outlook.com>
2020-08-13 21:11:57 -0300

    NLW#2 | CONCLUIDO

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const nunjucks = require('nunjucks');
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages');
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages');
 
 nunjucks.configure('src/views',{
   express: server,
@@ -9,10 +9,12 @@ nunjucks.configure('src/views',{
 })
 
 server
+.use(express.urlencoded({extended: true}))
 .use(express.static("public"))
 
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
 .listen(5500)
--- src/utils/format.js
+++ src/utils/format.js
@@ -26,8 +26,14 @@ const position = +subjectNumber - 1
 return subjects[position]
 }
 
+function convertHoursToMinutes(time){
+  const [hour, minutes] = time.split(":")
+  return Number((hour * 60) + minutes)
+}
+
 module.exports = {
   subjects,
   weekdays,
-  getSubject
+  getSubject,
+  convertHoursToMinutes
 }
